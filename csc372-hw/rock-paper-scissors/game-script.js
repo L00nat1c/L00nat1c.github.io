d0f26a7f444cc2281dcd7023be63088f352c725d
Brandon Looney <bmlooney@uncg.edu>
2025-03-06 15:57:11 -0500

    attempting javascript now

---
+++ csc372-hw/rock-paper-scissors/game-script.js
@@ -0,0 +1,17 @@
+const choices = ["rock", "paper", "scissors"];
+const allPictures = ["\csc372-hw\rock-paper-scissors\game-images\rock.PNG",
+    "\csc372-hw\rock-paper-scissors\game-images\paper.PNG",
+    "\csc372-hw\rock-paper-scissors\game-images\scissors.PNG",
+    "\csc372-hw\rock-paper-scissors\game-images\question-mark.PNG"
+]
+
+function game() {
+    
+
+    function playGame() {
+        const rockBut = document.querySelector('#rock');
+        const papBut = document.querySelector('#paper');
+        const sciBut = document.querySelector('#scissors');
+        const playerOpt = [rockBut, papBut, sciBut];
+    }
+}
