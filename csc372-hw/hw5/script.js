3287324d128411f4b2405e7fd25484222f87ee54
Brandon Looney <bmlooney@uncg.edu>
2025-03-21 19:32:22 -0400

    going insane, nothing works, don't know what I'm doing, don't know what to do (I'm not going to the teacher because I can't understand her), blah blah blah, I'm giving up and going to dinner

--- csc372-hw/hw5/script.js
+++ csc372-hw/hw5/script.js
@@ -1,7 +1,7 @@
-//const apiUrl = "https://api.github.com/users/L00nat1c/repos";
+const apiUrl = "https://api.github.com/users/L00nat1c/repos";
 
 //tryed to use this from a YouTube video on how to display API data on HTML
-const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+//const apiUrl = 'https://jsonplaceholder.typicode.com/users';
 
 function getRepos() {
     fetch(apiUrl)
@@ -14,7 +14,7 @@ function getRepos() {
 
     .then((repoData) => {
         repoData.forEach(element => {
-            const markup = '<li>${user.name}</li>';
+            const markup = '<li>${repos.name}</li>';
 
             document.querySelector('ul').insertAdjacentHTML('beforeend', markup);
         });
